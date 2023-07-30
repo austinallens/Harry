0bee8ce14eccfd348c22325869ef69187c7c2d61
Austin Allen <95332894+austinallens@users.noreply.github.com>
2023-07-30 01:32:20 -0500

    Add files via upload

---
+++ src/Harry.java
@@ -0,0 +1,24 @@
+/* Name: Austin Allen
+ * Course: CSC 135
+ * Project: Problem 1.3
+ * File Name: Harry.java
+ */
+
+public class Harry {
+
+    public static void main(String[] args) {
+        System.out.println("    ,/////\\\\,\n" //line 1
+                + "  ,///////\\\\\\\\\\\n"     //line 2
+                + ",//////   >  \\\\\\\\\n"    //line 3
+                + " ////  __ `    _\\\\\n"     //line 4
+                + "// __//  \\--//  \\\n"      //line 5
+                + "/' --\\\\_o/  \\\\_o/\n"    //line 6
+                + "\\_          \\   |\n"      //line 7
+                + "  \\       ,__>  /\n"       //line 8
+                + "  |\\    ,____  /\n"        //line 9
+                + "  | \\    \\__| /\n"        //line 10
+                + "  |   '._____/\n"           //line 11
+                + "  |       |");              //line 12
+    }
+    
+}
